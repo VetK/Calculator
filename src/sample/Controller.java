53c203d2cffd499464e9c4f0a95a52f796620925
Vitalii <re.test@outlook.com>
2018-07-03 21:17:11 +0300

    upgr

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,72 +1,14 @@
 package sample;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class Controller {
 
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
-    @FXML
-    private Button zero;        //0
-
-    @FXML
-    private Button one;         //1
-
-    @FXML
-    private Button two;         //2
-
-    @FXML
-    private Button three;       //3
-
-    @FXML
-    private Button four;        //4
-
-    @FXML
-    private Button five;        //5
-
-    @FXML
-    private Button six;         //6
-
-    @FXML
-    private Button seven;       //7
-
-    @FXML
-    private Button eight;       //8
-
-    @FXML
-    private Button nine;        //9
-
-    @FXML
-    private Button dpopping;    //скинуть введенное
-
     @FXML
     private Button percent;     //процент
-
-    @FXML
-    private Button divide;      //разделить
-
-    @FXML
-    private Button multiply;    //умножить
-
-    @FXML
-    private Button minus;       //отнять
-
-    @FXML
-    private Button plus;        //прибавить
-
-    @FXML
-    private Button comma;       //запятая
-
-    @FXML
-    private Button equally;     //равно
 
     @FXML
     private Text conclusion;    //дисплей вывода
@@ -84,86 +26,55 @@ public class Controller {
 
     private double vuvod = 0;
 
-    public static void knopka(){
+    @FXML
+    public void signs(ActionEvent event){               //для знаков "+", "-", "/", "*"
+        Button button=(Button)event.getSource();
+        symbol(button.getText());
+    }
 
+    @FXML
+    public void numerics(ActionEvent event){            //для цифр 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        Button button=(Button)event.getSource();
+        display(button.getText());
+        disp_test(button.getText());
+    }
+
+    @FXML
+    public void equals(ActionEvent event){               //для "="
+        Button button=(Button)event.getSource();
+        display(button.getText());
+        zahl_a = "";
+        resultat_show = getResultat_show(vuvod);
+        conclusion_2.setText(Double.toString(resultat_show));
+    }
+
+    @FXML
+    public void drop(ActionEvent event){                  //для "AC"
+        Text = "";
+        resultat_show = 0;
+        indik = " ";
+        zahl_a = "";
+        vuvod = 0;
+        conclusion.setText(Text);
+        conclusion_2.setText(Text);
+    }
+
+    @FXML
+    public void btComma(ActionEvent event){               //для "AC"
+        Button button=(Button)event.getSource();
+        if (!zahl_a.contains(button.getText())){
+            display(button.getText());
+            zahl_a = zahl_a.concat(button.getText());
+        }
     }
 
     @FXML
     void initialize() {
-        ArrayList<Button> numbers = new ArrayList<Button>();
-        numbers.add(zero);
-        numbers.add(one);
-        numbers.add(two);
-        numbers.add(three);
-        numbers.add(four);
-        numbers.add(five);
-        numbers.add(six);
-        numbers.add(seven);
-        numbers.add(eight);
-        numbers.add(nine);
-
-        for (int i=0; i<numbers.size(); i++){
-            Button b = numbers.get(i);
-            numbers.get(i).setOnAction(event -> {
-                display(b.getText());
-                disp_test(b.getText());
-            });
-        }
-
-        divide.setOnAction(event -> {
-            symbol("/");
-
-        });
-
-        multiply.setOnAction(event -> {
-            symbol("*");
-
-        });
-
-        minus.setOnAction(event -> {
-            symbol("-");
-
-        });
-
-        plus.setOnAction(event -> {
-            symbol("+");
-
-        });
-
         percent.setOnAction(event -> { // дописать метод для %
             //symbol("%");
 
         });
 
-        equally.setOnAction(event -> {
-            display("=");
-            zahl_a = "";
-            resultat_show = getResultat_show(vuvod);
-            conclusion_2.setText(Double.toString(resultat_show));
-
-        });
-
-        dpopping.setOnAction(event -> {
-            Text = "";
-            conclusion.setText(Text);
-            conclusion_2.setText(Text);
-
-            resultat_show = 0;
-
-            indik = " ";
-
-            zahl_a = "";
-
-            vuvod = 0;
-
-        });
-
-        comma.setOnAction(event -> {
-            if (!zahl_a.contains(".")){
-                display(".");
-                zahl_a = zahl_a.concat(".");
-            }
-        });
 
     }
 
