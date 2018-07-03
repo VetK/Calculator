d35c6445cea1dade9d62d28354fb2eccede39017
Vitalii <re.test@outlook.com>
2018-07-03 21:57:03 +0300

    upgr

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,26 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
-public class Controller {
+public class Controller extends InitController{
 
     @FXML
     private Button percent;     //процент
 
     @FXML
-    private Text conclusion;    //дисплей вывода
+    static Text conclusion;    //дисплей вывода
 
     @FXML
-    private Text conclusion_2;  //дисплей вывода результат
-
-    private static String Text = "";
-
-    public static double resultat_show = 0;
-
-    private String indik = " ";
-
-    private String zahl_a = "";
-
-    private double vuvod = 0;
+    static Text conclusion_2;  //дисплей вывода результата
 
     @FXML
     public void signs(ActionEvent event){               //для знаков "+", "-", "/", "*"
@@ -76,59 +66,6 @@ public class Controller {
         });
 
 
-    }
-
-    private double getResultat_show (double number){      //делает вычисление
-        switch (indik){
-            case "-":
-                resultat_show = resultat_show - number;
-                return resultat_show;
-            case "+":
-                resultat_show = resultat_show + number;
-                return resultat_show;
-            case "/":
-                resultat_show = resultat_show / number;
-                return resultat_show;
-            case "*":
-                resultat_show = resultat_show * number;
-                return resultat_show;
-            case " ":
-                resultat_show = number;
-                return resultat_show;
-        }
-        return 0;
-    }
-
-    private void display (String operation){
-        if(!Text.endsWith("=")){
-            Text = Text.concat(operation);
-            conclusion.setText(Text);
-        }
-    }
-
-    private void disp_test(String te){
-        zahl_a = zahl_a.concat(te);
-        vuvod = Double.valueOf(zahl_a);
-    }
-
-    private void symbol(String symb){         //перезаписывает математический знак и делает вычисления
-
-        if (Text.endsWith(symb)) {
-
-        }
-        else if (Text.endsWith("-") || Text.endsWith("+") || Text.endsWith("*") || Text.endsWith("/") || Text.endsWith("=")) {
-            indik = symb;
-            int a = Text.length() - 1;
-            Text = Text.substring(0, a).concat(symb);
-            conclusion.setText(Text);
-        }
-        else {
-            display(symb);
-            zahl_a = "";
-            resultat_show = getResultat_show(vuvod);
-            indik = symb;
-            conclusion_2.setText(Double.toString(resultat_show));
-        }
     }
 
 }
